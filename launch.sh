acc52423432f26c13edf80523c9c117c86ffb728
kz6fittycent <jimmy.tigert@gmail.com>
2020-10-23 15:20:55 -0500

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -5,7 +5,7 @@
 TSD="$SNAP_USER_DATA/TunerStudioMS"
 TSL="TunerStudio.tar.gz https://www.efianalytics.com/TunerStudio/download/TunerStudioMS_v3.1.04.tar.gz"
 TS="TunerStudioMS_v3.1.04.tar.gz"
-
+SERIAL_DRIVER="${TSD}/lib/alternateLinuxDrivers/x86_64-linux:"
 ########################################
 
 # JAVA PATHS
@@ -17,24 +17,28 @@ export PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH
 
 # GET TunerStudio if it doesn't exist then START
 
-if [ -e ${TSD} ];
+if [ -e "${TSD}" ];
 
 then
-  
+  # CLEAN UP
+  rm -rf ${TS}
   # START
-  java -jar -Dcom.ibm.crypto.provider.DoRSATypeChecking=false ${TSD}/TunerStudioMS.jar
+  cd ${TSD}
+  java -Dcom.ibm.crypto.provider.DoRSATypeChecking=false -Djava.library.path=${SERIAL_DRIVER}lib -cp ".:lib:plugins" -jar TunerStudioMS.jar
   
 else
 
   # GET TunerStudio
-  curl -so ${TS} ${TSL}
-  tar xvzf ${TS} -C ${TSD}
+#  mkdir -p ${TSD}
+  curl -so ${TSL}
+  tar xvzf ${TS}
   
   # CLEAN UP
   rm -rf ${TS}
   
   # START
-  java -jar -Dcom.ibm.crypto.provider.DoRSATypeChecking=false ${TSD}/TunerStudioMS.jar
+  cd ${TSD}
+  java -Dcom.ibm.crypto.provider.DoRSATypeChecking=false -Djava.library.path=${SERIAL_DRIVER}lib -cp ".:lib:plugins" -jar TunerStudioMS.jar TunerStudioMS.jar
   
 fi
 
