a0154078c5904867ff88c70a5e99c0132a1520ef
kz6fittycent <jimmy.tigert@gmail.com>
2020-10-23 14:09:07 -0500

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -2,7 +2,7 @@
 
 # VARIABLES
 ########################################
-TSD="$SNAP_USER_DATA/TunerStudioMS/"
+TSD="$SNAP_USER_DATA/TunerStudioMS"
 
 # JAVA PATHS
 ########################################
@@ -16,24 +16,21 @@ export PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH
 if [ -e ${TSD} ];
 
 then
-
-  cd ${TSD}
   
   # START
-  java -jar -Dcom.ibm.crypto.provider.DoRSATypeChecking=false TunerStudioMS.jar
+  java -jar -Dcom.ibm.crypto.provider.DoRSATypeChecking=false ${TSD}/TunerStudioMS.jar
   
 else
 
   # GET TunerStudio
-  wget -O TunerStudio.tar.gz https://www.efianalytics.com/TunerStudio/download/TunerStudioMS_v3.1.04.tar.gz
+  curl -so TunerStudio.tar.gz https://www.efianalytics.com/TunerStudio/download/TunerStudioMS_v3.1.04.tar.gz
   tar xvzf TunerStudio.tar.gz
   
   # CLEAN UP
   rm -rf TunerStudio.tar.gz
   
   # START
-  cd ${TSD}
-  java -jar -Dcom.ibm.crypto.provider.DoRSATypeChecking=false TunerStudioMS.jar
+  java -jar -Dcom.ibm.crypto.provider.DoRSATypeChecking=false ${TSD}/TunerStudioMS.jar
   
 fi
 
